c4a4f24d2379897e001589eae0f03e295309d613
youssef sk <alsarakibiy@gmail.com>
2022-03-11 17:47:07 +0200

    fixed the nav

--- js/index.js
+++ js/index.js
@@ -6,49 +6,39 @@ menuBtn.addEventListener("click", function () {
 // slidShow
 let arrowBtn = document.querySelectorAll("[data-arr]");
 let landing = document.querySelector("[data-landing]");
-let arrNum = 0;
-let imgArr = [
-	"../image/landing(1).jpg",
-	"../image/landing(2).jpg",
-	"../image/landing(3).jpg",
-];
-let bullets = document.querySelectorAll("[data-bullets] > *");
-arrowBtn.forEach((el) => {
-	el.addEventListener("click", (e) => {
-		e.target.dataset.arr === "after" ? arrNum++ : arrNum--;
-		if (arrNum < 0) {
-			arrNum = imgArr.length - 1;
+let landingImg = document.querySelectorAll("[data-img]");
+let bullets = document.querySelectorAll("[data-bullets]");
+let imgNum = 0;
+
+arrowBtn.forEach((el) =>
+	el.addEventListener("click", (el) => {
+		console.log(imgNum);
+		el.target.getAttribute("data-arr") === "after" ? (imgNum += 1) : (imgNum += -1);
+		if (imgNum < 0) {
+			imgNum = landingImg.length - 1;
+		} else if (imgNum > landingImg.length - 1) {
+			imgNum = 0;
 		}
-		if (arrNum > imgArr.length - 1) {
-			arrNum = 0;
-		}
-		landing.style.backgroundImage = ` url('${imgArr[arrNum]}')`;
-		// console.log(bullets);
-		bullets.forEach((el) => {
-			el.classList.remove("active");
-		});
-		bullets[arrNum].classList.add("active");
-	});
-});
+		landingImg.forEach((el) => el.classList.remove("active"));
+		landingImg[imgNum].classList.add("active");
+		bullets.forEach((el) => el.classList.remove("active"));
+		bullets[imgNum].classList.add("active");
+	})
+);
+
 // nav
 
 let nav = document.querySelector("[data-nav]");
 let links = document.querySelectorAll("[data-links");
 let linksSection = document.querySelectorAll("[data-section]");
-let observerBack = new IntersectionObserver((el) => {
-	if (el.isIntersection) return;
-	else nav.classList.toggle("in");
-}, {});
+let observerBack = new IntersectionObserver(
+	(el) => {
+		if (el.isIntersection === true) return;
+		else nav.classList.toggle("in");
+	},
+	{
+		threshold: 0.5,
+	}
+);
 
 observerBack.observe(landing);
-
-let navObserverSection = new IntersectionObserver((el) => {
-	if (!el.isIntersection) links.forEach((el) => el.classList.remove("active"));
-	el.forEach((el) => {
-		if (el.isIntersecting) {
-			document.getElementById(el.target.dataset.section).classList.add("active");
-			console.log(el.target.dataset.section);
-		}
-	});
-}, {});
-linksSection.forEach((el) => navObserverSection.observe(el));
